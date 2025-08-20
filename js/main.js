b5580b237ca23cedd37697629738168f79c4ed23
Mahmoud Najmeh <mamocool3@gmail.com>
2025-08-20 15:13:43 +0200

    Disable F12 and Ctrl+Shift+I

--- js/main.js
+++ js/main.js
@@ -332,3 +332,13 @@ indicatorsContainer.addEventListener('click', (e) => {
 handleScroll();
 
 });
+
+// Disable right-click
+document.addEventListener('contextmenu', (event) => event.preventDefault());
+
+// Disable F12 and Ctrl+Shift+I
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
+        event.preventDefault();
+    }
+});
