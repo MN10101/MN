0b3737cf887d6c4e39b70793741a8a5cdf927363
Mahmoud Najmeh <mamocool3@gmail.com>
2025-08-21 19:53:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -332,13 +332,3 @@ indicatorsContainer.addEventListener('click', (e) => {
 handleScroll();
 
 });
-
-// Disable right-click
-document.addEventListener('contextmenu', (event) => event.preventDefault());
-
-// Disable F12 and Ctrl+Shift+I
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
-        event.preventDefault();
-    }
-});
